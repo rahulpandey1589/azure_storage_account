79bcdd469af251b06d93699772b7f8662fdb05e0
Rahul Pandey <rahulpandey862@gmail.com>
2022-02-19 14:13:21 +0530

    Initial Checkin

---
+++ azure_storage.clientapplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using azure_storage.core.BlobStorage;
+
+namespace azure_storage.clientapplication
+{
+    class Program
+    {
+        static async  Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BlobOperations operations
+                = new BlobOperations();
+
+
+            await operations.CreateBlobContainerAsync("new-container");
+
+            await operations.UploadBlobAsync("new-container", "container-blobs", "Big-data-Training-Plan (2).docx");
+        }
+    }
+}
---
+++ azure_storage.core/BlobStorage/BlobOperations.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace azure_storage.core.BlobStorage
+{
+    public class BlobOperations
+    {
+
+        private string connectionString
+             = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+        public async Task CreateBlobContainerAsync(
+            string containerName)
+        {
+            try
+            {
+                var blobServiceClient = CreateBlobServiceClient();
+
+                var data =
+                    await blobServiceClient.CreateBlobContainerAsync(containerName).ConfigureAwait(false);
+            }
+            catch (Exception) { }
+        }
+
+        public async Task UploadBlobAsync(
+            string containerName,
+            string rootFolderName,
+            string fileName)
+        {
+            var blobContainerClient =
+                CreateBlobContainerClient(CreateBlobServiceClient(), containerName);
+
+            var completePath =
+                  $"{ rootFolderName }//{ Path.GetFileNameWithoutExtension(fileName)}" +
+                   $"{Guid.NewGuid().ToString()}{Path.GetExtension(fileName)}";
+
+            var blobClient = blobContainerClient.GetBlobClient(completePath);
+
+            var localPath = "E:\\" + fileName;
+
+            await blobClient.UploadAsync(localPath);
+        }
+
+
+        private BlobServiceClient CreateBlobServiceClient()
+        {
+            return new BlobServiceClient(connectionString);
+        }
+
+        private BlobContainerClient CreateBlobContainerClient(
+            BlobServiceClient blobServiceClient,
+            string containerName)
+        {
+            return blobServiceClient.GetBlobContainerClient(containerName);
+        }
+    }
+}
